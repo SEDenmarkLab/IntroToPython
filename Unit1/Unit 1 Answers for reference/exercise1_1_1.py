486ce1c86697377302c40871a3dd68bf8f27953b
wentianhe <hwt070810@outlook.com>
2026-09-24 14:17:21 -0500

    Unit 1 answer key uploaded

---
+++ Unit1/Unit 1 Answers for reference/exercise1_1_1.py
@@ -0,0 +1,15 @@
+from math import pi
+def area(radius):
+    """Calculate the area of a circle
+
+    Parameters
+    ----------
+    radius : int
+        The radius of the circle in integer
+
+    Returns
+    -------
+    int
+        The calculated area will be returned in integer
+    """
+    return pi * radius * radius
